fac9dff4ec602e3dcaa1bf92a1ab290d533a89c3
cuchillozapallero@gmail.com <cuchillozapallero@gmail.com@8eee64fc-b93a-0410-862f-31c98061876f>
2007-10-12 03:14:25 +0000

    Archivos del proyecto

---
+++ trunk/Parsers/src/parsers/sax/Attributes.java
@@ -0,0 +1,18 @@
+/*
+ * Attributes.java
+ * 
+ * Created on 12/10/2007, 00:11:44
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package parsers.sax;
+
+/**
+ *
+ * @author GabrieL
+ */
+class Attributes {
+
+}
