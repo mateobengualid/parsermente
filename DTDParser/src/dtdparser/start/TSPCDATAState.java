fd89735d1f0ae6f1506f64644e48c5a67b9b1d1d
apoteoticos <apoteoticos@8eee64fc-b93a-0410-862f-31c98061876f>
2008-07-22 11:01:28 +0000

    Parser DTD, el modelo está en otra parte

---
+++ DTDParser/src/dtdparser/start/TSPCDATAState.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dtdparser.start;
+
+/**
+ *
+ * @author mateo
+ */
+public class TSPCDATAState {
+
+}
