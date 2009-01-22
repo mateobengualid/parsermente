cc19e26c06ede753105d06cb6d72d1dec82c7df4
apoteoticos <apoteoticos@8eee64fc-b93a-0410-862f-31c98061876f>
2009-01-22 11:51:15 +0000

    Saqué los tags de UML Editor

--- trunk/Parsers/src/parsers/sax/SAXHandler.java
+++ trunk/Parsers/src/parsers/sax/SAXHandler.java
@@ -12,36 +12,21 @@ package parsers.sax;
  *     
  *     Clase que no hace nada, destinada a ser heredada y reescrita
  */
-// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-// #[regen=yes,id=DCE.1C789B16-EC96-2DCA-DE4E-3D59694EC1BE]
-// </editor-fold> 
+
 public class SAXHandler
 {
-
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.BCE5469A-4D9E-47D9-0654-3DB4F18E59FA]
-    // </editor-fold> 
     public void startDocument()
     {
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.03945475-B64A-2021-05AB-A03B028A27BF]
-    // </editor-fold> 
     public void endDocument()
     {
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.41585413-022E-2100-6308-0C1FC5D6EC13]
-    // </editor-fold> 
     public void startElement(String name, Attributes atts)
     {
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.557C3307-4383-C4DE-7A42-BDF92E95B8BE]
-    // </editor-fold> 
     public void endElement(String name)
     {
     }
