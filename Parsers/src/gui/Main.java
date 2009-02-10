c3faf0881d0c37b10428e90306d47e3010b293a5
cuchillozapallero <cuchillozapallero@8eee64fc-b93a-0410-862f-31c98061876f>
2009-02-10 05:08:00 +0000

    Implementacion de GUI y agregacion de xml para test.

---
+++ Parsers/src/gui/Main.java
@@ -0,0 +1,31 @@
+/*
+ * Main.java
+ * 
+ * Created on 23-sep-2007, 13:50:24
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+/**
+ *  @author Mateo
+ */
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.C35BFFB3-BE2F-251A-3EBF-26DDB6F12A1D]
+// </editor-fold> 
+public class Main
+{
+    /**
+     *  @param args the command line arguments
+     */
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.983D08A2-CB50-F4D8-2269-E1E590CE679A]
+    // </editor-fold> 
+    public static void main(String[] args)
+    {
+        frmMain frame = new frmMain();
+        
+        frame.setVisible(true);                               
+    }    
+}
